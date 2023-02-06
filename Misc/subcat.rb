22d1dc732f8ed6aeaf0f4fc7bf2e692d3bb55d73
Space Banana <tomasbessa@protonmail.com>
2023-02-06 01:03:12 +0000

    Add files via upload

---
+++ Misc/subcat.rb
@@ -0,0 +1,33 @@
+def print_containing_subtext (filename, subtext)
+    linecount = 1
+    anyfound = 0
+    file = File::open(filename,  "r")
+    for line in file.readlines do
+        if line.include?(subtext) == true
+            puts "#{linecount}: #{line}"
+            anyfound+=1
+        end
+        linecount+=1
+    end
+    if anyfound == 0
+        puts "No lines containing the substring '#{subtext}' have been found"
+    end
+end
+
+shell_args = ARGV
+shell_args_length = shell_args.length
+
+if shell_args_length >= 2 && File::exist?(shell_args[0]) == true
+    filename = shell_args[0]
+    subtext = shell_args[1]
+else
+    ARGV.clear
+    puts "Input file name"
+    filename = gets.chomp
+    if File::exist?(filename) == false
+        puts "The file '#{filename}' does not exist"; return
+    end
+    puts "Input substring"
+    subtext = gets.chomp
+end
+print_containing_subtext(filename, subtext)
---
+++ Security/passgen.rb
@@ -0,0 +1,64 @@
+def passgen_ascii(passlen)
+    password=""
+   for i in 0..passlen
+        randomgen = Random.new
+        character = randomgen.rand(33..127).chr
+        password += character
+   end
+   return password
+end
+
+def passgen_dictionary(passlen, dictionaryname)
+    if File::exist?(dictionaryname) == false
+        puts "The dictionary '#{dictionaryname}' does not exist!"
+        return
+    end
+
+    password=""
+    dictionary = File::open(dictionaryname)
+    lines = dictionary.readlines
+    lines_amt = lines.length
+    chosen_keywords = Array.new(passlen)
+
+    for i in 0..passlen-1
+        randomgen = Random.new
+        chosen_keywords[i] = lines[randomgen.rand(0..lines_amt-1)].chop
+    end
+
+    for i in chosen_keywords
+        password = password + i + " "
+    end
+    return password
+end
+
+shell_args = ARGV
+shell_args_length = shell_args.length
+if shell_args_length >= 2
+    if shell_args[0] == "ascii"
+        mode = 1
+        puts passgen_ascii(shell_args[1])
+    elsif shell_args[0] == "dict"
+        mode = 2
+        puts passgen_dictionary(shell_args[1], shell_args[2])
+    end
+else
+    ARGV.clear
+    puts "0. Exit     1. ASCII password     2. Dictionary password"
+    puts "Choose a mode"; gets mode
+    numbers="012"
+    if numbers.include?(mode) == false
+        puts "You need to select one of the available modes!"
+        return
+    end
+    mode = mode.to_i
+    puts "Input password length"
+    passlength = gets.chomp
+    case mode
+    when 1
+        puts passgen_ascii(passlength)
+    when 2
+        puts "Input dictionary file name"
+        dictname = gets.chomp
+        puts passgen_dictionary(passlength, dictname)
+    end
+end
