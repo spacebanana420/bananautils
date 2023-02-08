1441132d5eb489d3f19312133d0bdf04b0a371ef
Space Banana <tomasbessa@protonmail.com>
2023-02-08 12:01:40 +0000

    crystalflate: improvements and fixes

--- File utils/crystalflate.rb
+++ File utils/crystalflate.rb
@@ -6,18 +6,29 @@ def deflate_file (filename, level)
     end
     input_file = File::read(filename)
     output_file = Zlib::Deflate.deflate(input_file, level)
-    puts "#{infile} compressed"; puts "Original size: #{input_file.size}    Compressed size: #{output_file.size}"
+    puts "#{input_file} compressed"; puts "Original size: #{input_file.size}    Compressed size: #{output_file.size}"
     File.write("#{filename}.cfl", output_file)
 end
 
-def inflate_file (filename, level)
+def inflate_file (filename)
+    filename_noext = remove_extension(filename, ".cfl")
     if check_overwrite(filename_noext) == false
         return
     end
-    filename_noext = remove_extension(filename, ".cfl")
     input_file = File::read(filename)
     output_file = Zlib::Inflate.inflate(input_file)
     File.write(filename_noext, output_file)
+end
+
+def get_compression_level ()
+    numbers="0123456789"
+    puts "Choose a compression level (0-9)"
+    level = gets.chomp
+    if numbers.include?(level) == false
+        puts "You need to choose a level! Level has been defaulted to 3"
+        return 3
+    end
+    return level.to_i
 end
 
 def print_dir ()
@@ -69,7 +80,25 @@ print_dir()
 puts "Input file name"
 filename = gets.chomp
 if File::exist?(filename) == true
-    deflate_file(filename, 3)
+    puts "1. Compress    2. Decompress    3. Automatic (default)"
+    puts "Choose an operation"
+    operation = gets.chomp
+    if "123".include?(operation) == false
+        operation = 3
+    else
+        operation = operation.to_i
+    end
+    if operation == 1
+        deflate_file(filename, get_compression_level())
+    elsif operation == 2
+        inflate_file(filename)
+    else
+        if filename.include?(".cfl") == false
+            deflate_file(filename, get_compression_level())
+        else
+            inflate_file(filename)
+        end
+    end
 else
     puts "The specified file does not exist"
 end
